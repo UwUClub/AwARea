b475da2e951ceb68858aeb56a2906e3d0d21051e
Glassalo <maxence.labourel@epitech.eu>
2023-11-27 11:08:29 +0100

    Add Swagger documentation to API

--- back/index.js
+++ back/index.js
@@ -1,11 +1,32 @@
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
 const express = require('express')
-const app = express()
-const port = 8080
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+        title: 'Express API for JSONPlaceholder',
+        version: '1.0.0',
+    },
+};
+
+const options = {
+    swaggerDefinition,
+    // Paths to files containing OpenAPI definitions
+    apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options)
+
+var app = express()
+
+const port = 8080
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
