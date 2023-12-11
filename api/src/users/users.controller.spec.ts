2d168e7f08ad27c6353f29b7d51e30b3dd22b43c
Glassalo <maxence.labourel@epitech.eu>
2023-12-11 15:41:34 +0100

    Add tests for authentication endpoints

--- api/src/users/users.controller.spec.ts
+++ api/src/users/users.controller.spec.ts
@@ -1,20 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UsersRepository } from './users.repository';
+import { Model } from 'mongoose';
+import { UsersMapper } from './users.mapper';
 
 describe('UsersController', () => {
     let controller: UsersController;
+    let service: UsersService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [UsersController],
-            providers: [UsersService],
+            providers: [
+                UsersService,
+                UsersMapper,
+                UsersRepository,
+                {
+                    provide: 'UserModel',
+                    useValue: Model,
+                },
+            ],
         }).compile();
 
         controller = module.get<UsersController>(UsersController);
+        service = module.get<UsersService>(UsersService);
     });
 
     it('should be defined', () => {
         expect(controller).toBeDefined();
+        expect(service).toBeDefined();
     });
 });
--- api/src/users/users.service.spec.ts
+++ api/src/users/users.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { UsersRepository } from './users.repository';
+import { Model } from 'mongoose';
 
 describe('UsersService', () => {
     let service: UsersService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [UsersService],
+            providers: [
+                UsersService,
+                UsersRepository,
+                {
+                    provide: 'UserModel',
+                    useValue: Model,
+                },
+            ],
         }).compile();
 
         service = module.get<UsersService>(UsersService);
