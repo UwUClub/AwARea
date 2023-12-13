6a4a2bc6ab8bc102050dfdb274dfc0af9afb605a
StEgo2103 <lucadeltort23@gmail.com>
2023-12-13 11:49:05 +0100

    Add navbar mobile view

--- flutter_area/lib/UI/Home/home_view.dart
+++ flutter_area/lib/UI/Home/home_view.dart
@@ -7,10 +7,11 @@ import '../../Core/Locator/locator.dart';
 import '../../Core/Manager/theme_manager.dart';
 import '../../Utils/Extensions/color_extensions.dart';
 import '../../Utils/Extensions/double_extensions.dart';
+import '../../Utils/constants.dart';
 import '../ReusableWidgets/mk_background.dart';
-import '../ReusableWidgets/mk_switch.dart';
 
 import '../Settings/settings_view.dart';
+import 'home_mobile_view.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -32,120 +33,219 @@ class _HomeViewState extends State<HomeView> {
     super.initState();
   }
 
+  int currentPageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return MkBackground(child: Scaffold(
-      backgroundColor: Theme.of(context).brightness == Brightness.light
-                        ? Theme.of(context).colorScheme.lightColor1
-                        : Theme.of(context).colorScheme.darkColor1,
-      body: Row(
-        children: [
-          SideMenu(
-            controller: sideMenu,
-            style: SideMenuStyle(
-              displayMode: SideMenuDisplayMode.open,
-              compactSideMenuWidth: 60,
-              hoverColor: Theme.of(context).brightness == Brightness.light
-                                  ? Theme.of(context).colorScheme.lightColor3
-                                  : Theme.of(context).colorScheme.darkColor3,
-              selectedHoverColor: Theme.of(context).brightness == Brightness.light
-                                  ? Theme.of(context).colorScheme.lightColor3
-                                  : Theme.of(context).colorScheme.darkColor3,
-              selectedColor: Theme.of(context).brightness == Brightness.light
-                                  ? Theme.of(context).colorScheme.lightColor4
-                                  : Theme.of(context).colorScheme.darkColor4,
-              selectedTitleTextStyle: Theme.of(context).brightness == Brightness.light
-                                  ? TextStyle(color: Theme.of(context).colorScheme.darkColor2)
-                                  : TextStyle(color: Theme.of(context).colorScheme.lightColor2),
-              unselectedTitleTextStyle: Theme.of(context).brightness == Brightness.light
-                                  ? TextStyle(color: Theme.of(context).colorScheme.darkColor2)
-                                  : TextStyle(color: Theme.of(context).colorScheme.lightColor2),
-              selectedIconColor: Theme.of(context).brightness == Brightness.light
-                                  ? Theme.of(context).colorScheme.darkColor2
-                                  : Theme.of(context).colorScheme.lightColor2,
-              unselectedIconColor: Theme.of(context).brightness == Brightness.light
-                                  ? Theme.of(context).colorScheme.darkColor2
-                                  : Theme.of(context).colorScheme.lightColor2,
-              backgroundColor: Theme.of(context).brightness == Brightness.light
-                                  ? Theme.of(context).colorScheme.lightColor2
-                                  : Theme.of(context).colorScheme.darkColor2,
-              toggleColor: Theme.of(context).brightness == Brightness.light
-                                  ? Theme.of(context).colorScheme.darkColor2
-                                  : Theme.of(context).colorScheme.lightColor2,
-              itemInnerSpacing: 13.0,
-            ),
-            showToggle: true,
-            title: Row(
-              children: [  
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: SvgPicture.asset('assets/images/Logo.svg',
-                  semanticsLabel: 'Logo',
-                  width: 28.0.ratioW(),
-                  height: 28.0.ratioH(),
-                  )
+    if (kIsPc) {
+      return MkBackground(
+          child: Scaffold(
+        backgroundColor: Theme.of(context).brightness == Brightness.light
+            ? Theme.of(context).colorScheme.lightColor1
+            : Theme.of(context).colorScheme.darkColor1,
+        body: Row(
+          children: <Widget>[
+            SideMenu(
+              controller: sideMenu,
+              style: SideMenuStyle(
+                displayMode: SideMenuDisplayMode.open,
+                compactSideMenuWidth: 60,
+                hoverColor: Theme.of(context).brightness == Brightness.light
+                    ? Theme.of(context).colorScheme.lightColor3
+                    : Theme.of(context).colorScheme.darkColor3,
+                selectedHoverColor:
+                    Theme.of(context).brightness == Brightness.light
+                        ? Theme.of(context).colorScheme.lightColor3
+                        : Theme.of(context).colorScheme.darkColor3,
+                selectedColor: Theme.of(context).brightness == Brightness.light
+                    ? Theme.of(context).colorScheme.lightColor4
+                    : Theme.of(context).colorScheme.darkColor4,
+                selectedTitleTextStyle: Theme.of(context).brightness ==
+                        Brightness.light
+                    ? TextStyle(color: Theme.of(context).colorScheme.darkColor2)
+                    : TextStyle(
+                        color: Theme.of(context).colorScheme.lightColor2),
+                unselectedTitleTextStyle: Theme.of(context).brightness ==
+                        Brightness.light
+                    ? TextStyle(color: Theme.of(context).colorScheme.darkColor2)
+                    : TextStyle(
+                        color: Theme.of(context).colorScheme.lightColor2),
+                selectedIconColor:
+                    Theme.of(context).brightness == Brightness.light
+                        ? Theme.of(context).colorScheme.darkColor2
+                        : Theme.of(context).colorScheme.lightColor2,
+                unselectedIconColor:
+                    Theme.of(context).brightness == Brightness.light
+                        ? Theme.of(context).colorScheme.darkColor2
+                        : Theme.of(context).colorScheme.lightColor2,
+                backgroundColor:
+                    Theme.of(context).brightness == Brightness.light
+                        ? Theme.of(context).colorScheme.lightColor2
+                        : Theme.of(context).colorScheme.darkColor2,
+                toggleColor: Theme.of(context).brightness == Brightness.light
+                    ? Theme.of(context).colorScheme.darkColor2
+                    : Theme.of(context).colorScheme.lightColor2,
+                itemInnerSpacing: 13.0,
+              ),
+              showToggle: true,
+              title: Row(
+                children: <Widget>[
+                  Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: SvgPicture.asset(
+                        'assets/images/Logo.svg',
+                        semanticsLabel: 'Logo',
+                        width: 28.0.ratioW(),
+                        height: 28.0.ratioH(),
+                      )),
+                ],
+              ),
+              items: <SideMenuItem>[
+                SideMenuItem(
+                  title: AppLocalizations.of(context)!.profile,
+                  onTap: (int index, _) {
+                    sideMenu.changePage(index);
+                  },
+                  icon: const Icon(Icons.person),
                 ),
-              ],  
-            ),
-            items: [
-              SideMenuItem(
-                title: AppLocalizations.of(context)!.profile,
-                onTap: (int index, _) {
-                  sideMenu.changePage(index);
-                },
-                icon: const Icon(Icons.person),
-              ),
-              SideMenuItem(
-                title: AppLocalizations.of(context)!.settings,
-                onTap: (int index, _) {
-                  sideMenu.changePage(index);
-                },
-                icon: const Icon(Icons.settings),
-              ),
-              SideMenuItem(
-                title: AppLocalizations.of(context)!.connection,
-                onTap: (int index, _) {
-                  sideMenu.changePage(index);
-                },
-                icon: const Icon(Icons.share),
-              ),
-
-              SideMenuItem(
-                title: AppLocalizations.of(context)!.newTask,
-                onTap: (int index, _) {
-                  sideMenu.changePage(index);
-                },
-                icon: const Icon(Icons.add),
-              ),
-              SideMenuItem(
-                builder: (BuildContext context, SideMenuDisplayMode displayMode) {
-                  return const Divider(
-                    endIndent: 8,
-                    indent: 8,
-                  );
-                },
-              ),
-            ],
-          ),
-          Expanded(
-            child: PageView(
-              controller: pageController,
-              children: [
-                Center(
-                  child: Text(AppLocalizations.of(context)!.profile),
+                SideMenuItem(
+                  title: AppLocalizations.of(context)!.settings,
+                  onTap: (int index, _) {
+                    sideMenu.changePage(index);
+                  },
+                  icon: const Icon(Icons.settings),
                 ),
-                const SettingsView(),
-                Center(
-                  child: Text(AppLocalizations.of(context)!.connection),
+                SideMenuItem(
+                  title: AppLocalizations.of(context)!.connection,
+                  onTap: (int index, _) {
+                    sideMenu.changePage(index);
+                  },
+                  icon: const Icon(Icons.share),
                 ),
-                Center(
-                  child: Text(AppLocalizations.of(context)!.newTask),
+                SideMenuItem(
+                  title: AppLocalizations.of(context)!.newTask,
+                  onTap: (int index, _) {
+                    sideMenu.changePage(index);
+                  },
+                  icon: const Icon(Icons.add),
+                ),
+                SideMenuItem(
+                  builder:
+                      (BuildContext context, SideMenuDisplayMode displayMode) {
+                    return const Divider(
+                      endIndent: 8,
+                      indent: 8,
+                    );
+                  },
                 ),
               ],
             ),
-          ),
-        ],
-      ),
-    ));
+            Expanded(
+              child: PageView(
+                controller: pageController,
+                children: <Widget>[
+                  Center(
+                    child: Text(AppLocalizations.of(context)!.profile),
+                  ),
+                  const SettingsView(),
+                  Center(
+                    child: Text(AppLocalizations.of(context)!.connection),
+                  ),
+                  Center(
+                    child: Text(AppLocalizations.of(context)!.newTask),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ));
+    } else {
+      return MkBackground(
+          child: Scaffold(
+        backgroundColor: Theme.of(context).brightness == Brightness.light
+            ? Theme.of(context).colorScheme.lightColor1
+            : Theme.of(context).colorScheme.darkColor1,
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          height: 200.0.ratioH(),
+          backgroundColor: Theme.of(context).brightness == Brightness.light
+              ? Theme.of(context).colorScheme.lightColor2
+              : Theme.of(context).colorScheme.darkColor2,
+          indicatorColor: Theme.of(context).brightness == Brightness.light
+              ? Theme.of(context).colorScheme.lightColor4
+              : Theme.of(context).colorScheme.darkColor4,
+          selectedIndex: currentPageIndex,
+          destinations: <Widget>[
+            NavigationDestination(
+              label: '',
+              selectedIcon: Icon(Icons.home,
+                  color: Theme.of(context).brightness == Brightness.light
+                      ? Theme.of(context).colorScheme.darkColor1
+                      : Theme.of(context).colorScheme.lightColor1),
+              icon: Icon(Icons.home_outlined,
+                  color: Theme.of(context).brightness == Brightness.light
+                      ? Theme.of(context).colorScheme.lightColor4
+                      : Theme.of(context).colorScheme.darkColor4),
+            ),
+            NavigationDestination(
+              label: '',
+              selectedIcon: Icon(Icons.account_circle,
+                  color: Theme.of(context).brightness == Brightness.light
+                      ? Theme.of(context).colorScheme.darkColor1
+                      : Theme.of(context).colorScheme.lightColor1),
+              icon: Icon(Icons.account_circle_outlined,
+                  color: Theme.of(context).brightness == Brightness.light
+                      ? Theme.of(context).colorScheme.lightColor4
+                      : Theme.of(context).colorScheme.darkColor4),
+            ),
+            NavigationDestination(
+              label: '',
+              selectedIcon: Icon(Icons.settings,
+                  color: Theme.of(context).brightness == Brightness.light
+                      ? Theme.of(context).colorScheme.darkColor1
+                      : Theme.of(context).colorScheme.lightColor1),
+              icon: Icon(Icons.settings_outlined,
+                  color: Theme.of(context).brightness == Brightness.light
+                      ? Theme.of(context).colorScheme.lightColor4
+                      : Theme.of(context).colorScheme.darkColor4),
+            ),
+            NavigationDestination(
+              label: '',
+              selectedIcon: Icon(Icons.share,
+                  color: Theme.of(context).brightness == Brightness.light
+                      ? Theme.of(context).colorScheme.darkColor1
+                      : Theme.of(context).colorScheme.lightColor1),
+              icon: Icon(Icons.share_outlined,
+                  color: Theme.of(context).brightness == Brightness.light
+                      ? Theme.of(context).colorScheme.lightColor4
+                      : Theme.of(context).colorScheme.darkColor4),
+            ),
+            NavigationDestination(
+              label: '',
+              selectedIcon: Icon(Icons.add_circle,
+                  color: Theme.of(context).brightness == Brightness.light
+                      ? Theme.of(context).colorScheme.darkColor1
+                      : Theme.of(context).colorScheme.lightColor1),
+              icon: Icon(Icons.add_circle_outline,
+                  color: Theme.of(context).brightness == Brightness.light
+                      ? Theme.of(context).colorScheme.lightColor4
+                      : Theme.of(context).colorScheme.darkColor4),
+            ),
+          ],
+        ),
+        body: <Widget>[
+          const HomeMobileView(),
+          const Center(child: Text('Account')),
+          const Center(child: Text('Settings')),
+          const Center(child: Text('Connections')),
+          const Center(child: Text('Add')),
+        ][currentPageIndex],
+      ));
+    }
   }
 }
--- flutter_area/lib/Utils/constants.dart
+++ flutter_area/lib/Utils/constants.dart
@@ -1,3 +1,4 @@
 double kDeviceHeight = 0;
 double kDeviceWidth = 0;
 double kLargeScreenWidth = 800;
+bool kIsPc = false;
