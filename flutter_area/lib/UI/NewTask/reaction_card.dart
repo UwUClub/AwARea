3cbc7fdf11dfd098e15aa2a57b6dece79d9b11ec
Valentin Gegoux <geg.valentin@gmail.com>
2023-12-16 13:45:02 +0100

    Merge

--- flutter_area/lib/UI/NewTask/reaction_card.dart
+++ flutter_area/lib/UI/NewTask/reaction_card.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+
 import '../../Core/Manager/action_manager.dart';
 import '../../Utils/Extensions/color_extensions.dart';
 import '../../Utils/Extensions/double_extensions.dart';
-import 'reaction_selection.dart';
+import '../ReusableWidgets/mk_button.dart';
 
 class ReactionCard extends StatefulWidget {
   const ReactionCard({super.key, this.reaction, required this.setReaction});
@@ -33,20 +34,12 @@ class _ReactionCardState extends State<ReactionCard> {
       width: 312.0.ratioW(),
       height: 138.0.ratioH(),
       child: widget.reaction == null
-          ? ReactionSelection(
-              label: 'Google',
-              reactions: <MkReaction>[
-                MkReaction(
-                    service: 'Gmail',
-                    name: 'Send a mail',
-                    description: 'description'),
-                MkReaction(
-                    service: 'Drive',
-                    name: 'Upload a file',
-                    description: 'description'),
-              ],
-              setReaction: setReaction,
-            )
+          ? MkButton(
+              onPressed: () {
+                widget.setReaction(MkReaction(
+                    service: 'test', name: 'test', description: 'test'));
+              },
+              label: 'test')
           : Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
