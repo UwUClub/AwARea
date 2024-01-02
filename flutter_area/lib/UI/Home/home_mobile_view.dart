911ae5b21a4248999e415446f8c37b59ca004d8a
Valegox <geg.valentin@gmail.com>
2024-01-02 13:51:26 +0100

    Improve home page UI

--- flutter_area/lib/UI/Home/home_mobile_view.dart
+++ flutter_area/lib/UI/Home/home_mobile_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+import '../../Core/Locator/locator.dart';
+import '../../Core/Manager/user_manager.dart';
 import '../../Utils/Extensions/double_extensions.dart';
 
 class HomeMobileView extends StatefulWidget {
@@ -11,26 +13,27 @@ class HomeMobileView extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<HomeMobileView> {
+  UserManager userManager = locator<UserManager>();
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 40.0.ratioH()),
-      child: Center(
-        child: Row(
-          children: <Widget>[
-            SvgPicture.asset('assets/images/Logo.svg',
-                semanticsLabel: 'Logo',
-                width: 50.0.ratioW(),
-                height: 50.0.ratioH()),
-            const Spacer(),
-            Text('Bienvenue, MalvinDu87.',
-                style: Theme.of(context)
-                    .textTheme
-                    .titleLarge
-                    ?.copyWith(fontSize: 24)),
-          ],
-        ),
+    return SafeArea(
+        child: Align(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SvgPicture.asset('assets/images/Logo.svg',
+              semanticsLabel: 'Logo',
+              width: 50.0.ratioW(),
+              height: 50.0.ratioH()),
+          Text('Bienvenue,\n${userManager.fullName}.',
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge
+                  ?.copyWith(fontSize: 24),
+              textAlign: TextAlign.center),
+        ],
       ),
-    );
+    ));
   }
 }
