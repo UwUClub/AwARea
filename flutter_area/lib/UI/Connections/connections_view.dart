f61babc746d57f259a3a99369485be148b4a3ea3
Valegox <geg.valentin@gmail.com>
2024-01-02 18:33:22 +0100

    Fix UI for mobile devices

--- flutter_area/lib/UI/Connections/connections_view.dart
+++ flutter_area/lib/UI/Connections/connections_view.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../Core/Locator/locator.dart';
 import '../../Core/Manager/theme_manager.dart';
-import '../../Utils/Extensions/color_extensions.dart';
 import '../../Utils/Extensions/double_extensions.dart';
+import '../../Utils/constants.dart';
 import '../ReusableWidgets/mk_background.dart';
 import '../ReusableWidgets/mk_button.dart';
-import '../../Utils/constants.dart';
 
 class ConnectionsView extends StatefulWidget {
   const ConnectionsView({super.key});
@@ -26,119 +24,157 @@ class _ConnectionsViewState extends State<ConnectionsView> {
     if (kIsPc) {
       return MkBackground(
         child: Padding(
-            padding: EdgeInsets.only(
+          padding: EdgeInsets.only(
               top: 45.0.ratioH(),
               left: 137.0.ratioW(),
               right: 137.0.ratioW(),
               bottom: 36.0.ratioH()),
-              child: Column(
-                children: [
-                  Row(children: [
-                    Text(AppLocalizations.of(context)!.connection, style: Theme.of(context).textTheme.titleMedium)
-                    ]),
-                  const Divider(
-                      endIndent: 0,
-                      indent: 0,
-                    ),
-                  Row(children: [
-                    const Icon(FontAwesomeIcons.github, size: 20),
-                    Text(AppLocalizations.of(context)!.github, style: Theme.of(context).textTheme.labelLarge)
-                    ]),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(AppLocalizations.of(context)!.connectGithub, style: Theme.of(context).textTheme.labelMedium),
-                      MkButton(label: AppLocalizations.of(context)!.connect, onPressed: () {},),
-                    ],
-                  ),
-                  Row(children: [
-                    const Icon(FontAwesomeIcons.google, size: 20),
-                    Text(AppLocalizations.of(context)!.google, style: Theme.of(context).textTheme.labelLarge)
-                  ]),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(AppLocalizations.of(context)!.connectGoogle, style: Theme.of(context).textTheme.labelMedium),
-                      MkButton(label: AppLocalizations.of(context)!.connect, onPressed: () {},),
-                    ],
-                  ),
-                  Row(children: [
-                    const Icon(FontAwesomeIcons.slack, size: 20),
-                    Text(AppLocalizations.of(context)!.slack, style: Theme.of(context).textTheme.labelLarge)
-                  ]),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(AppLocalizations.of(context)!.connectSlack, style: Theme.of(context).textTheme.labelMedium),
-                      MkButton(label: AppLocalizations.of(context)!.connect, onPressed: () {},),
-                    ],
+          child: Column(
+            children: <Widget>[
+              Row(children: <Widget>[
+                Text(AppLocalizations.of(context)!.connection,
+                    style: Theme.of(context).textTheme.titleMedium)
+              ]),
+              const Divider(
+                endIndent: 0,
+                indent: 0,
+              ),
+              Row(children: <Widget>[
+                const Icon(FontAwesomeIcons.github, size: 20),
+                Text(AppLocalizations.of(context)!.github,
+                    style: Theme.of(context).textTheme.labelLarge)
+              ]),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(AppLocalizations.of(context)!.connectGithub,
+                      style: Theme.of(context).textTheme.labelMedium),
+                  MkButton(
+                    label: AppLocalizations.of(context)!.connect,
+                    onPressed: () {},
                   ),
                 ],
               ),
+              Row(children: <Widget>[
+                const Icon(FontAwesomeIcons.google, size: 20),
+                Text(AppLocalizations.of(context)!.google,
+                    style: Theme.of(context).textTheme.labelLarge)
+              ]),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(AppLocalizations.of(context)!.connectGoogle,
+                      style: Theme.of(context).textTheme.labelMedium),
+                  MkButton(
+                    label: AppLocalizations.of(context)!.connect,
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              Row(children: <Widget>[
+                const Icon(FontAwesomeIcons.slack, size: 20),
+                Text(AppLocalizations.of(context)!.slack,
+                    style: Theme.of(context).textTheme.labelLarge)
+              ]),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(AppLocalizations.of(context)!.connectSlack,
+                      style: Theme.of(context).textTheme.labelMedium),
+                  MkButton(
+                    label: AppLocalizations.of(context)!.connect,
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       );
     } else {
-      return MkBackground(
-        child: Padding(
+      return SafeArea(
+        child: MkBackground(
+          child: Padding(
             padding: EdgeInsets.only(
-              top: 45.0.ratioH(),
-              left: 37.0.ratioW(),
-              right: 37.0.ratioW(),
-              bottom: 36.0.ratioH()),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                    Text(AppLocalizations.of(context)!.connection, style: Theme.of(context).textTheme.titleMedium)
-                    ]),
-                  const Divider(
-                      endIndent: 0,
-                      indent: 0,
+                top: 45.0.ratioH(),
+                left: 37.0.ratioW(),
+                right: 37.0.ratioW(),
+                bottom: 36.0.ratioH()),
+            child: Column(
+              children: <Widget>[
+                Row(children: <Widget>[
+                  Text(AppLocalizations.of(context)!.connection,
+                      style: Theme.of(context).textTheme.titleMedium)
+                ]),
+                const Divider(
+                  endIndent: 0,
+                  indent: 0,
+                ),
+                Row(children: <Widget>[
+                  const Icon(FontAwesomeIcons.github, size: 20),
+                  Text(AppLocalizations.of(context)!.github,
+                      style: Theme.of(context).textTheme.labelLarge)
+                ]),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Flexible(
+                      child: Text(AppLocalizations.of(context)!.connectGithub,
+                          style: Theme.of(context).textTheme.labelMedium),
+                    )
+                  ],
+                ),
+                MkButton(
+                  label: AppLocalizations.of(context)!.connect,
+                  onPressed: () {},
+                ),
+                const Divider(
+                  endIndent: 0,
+                  indent: 0,
+                ),
+                Row(children: <Widget>[
+                  const Icon(FontAwesomeIcons.google, size: 20),
+                  Text(AppLocalizations.of(context)!.google,
+                      style: Theme.of(context).textTheme.labelLarge)
+                ]),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Flexible(
+                      child: Text(AppLocalizations.of(context)!.connectGoogle,
+                          style: Theme.of(context).textTheme.labelMedium),
                     ),
-                  Row(
-                    children: [
-                    const Icon(FontAwesomeIcons.github, size: 20),
-                    Text(AppLocalizations.of(context)!.github, style: Theme.of(context).textTheme.labelLarge)
-                    ]),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(AppLocalizations.of(context)!.connectGithub, style: Theme.of(context).textTheme.labelMedium)
-                    ],
-                  ),
-                  MkButton(label: AppLocalizations.of(context)!.connect, onPressed: () {},),
-                  const Divider(
-                    endIndent: 0,
-                    indent: 0,
-                  ),
-                  Row(children: [
-                    const Icon(FontAwesomeIcons.google, size: 20),
-                    Text(AppLocalizations.of(context)!.google, style: Theme.of(context).textTheme.labelLarge)
-                  ]),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(AppLocalizations.of(context)!.connectGoogle, style: Theme.of(context).textTheme.labelMedium),
-                    ],
-                  ),
-                  MkButton(label: AppLocalizations.of(context)!.connect, onPressed: () {},),
-                  const Divider(
-                    endIndent: 0,
-                    indent: 0,
-                  ),
-                  Row(children: [
-                    const Icon(FontAwesomeIcons.slack, size: 20),
-                    Text(AppLocalizations.of(context)!.slack, style: Theme.of(context).textTheme.labelLarge)
-                  ]),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(AppLocalizations.of(context)!.connectSlack, style: Theme.of(context).textTheme.labelMedium),
-                    ],
-                  ),
-                  MkButton(label: AppLocalizations.of(context)!.connect, onPressed: () {},),
-                ],
-              ),
+                  ],
+                ),
+                MkButton(
+                  label: AppLocalizations.of(context)!.connect,
+                  onPressed: () {},
+                ),
+                const Divider(
+                  endIndent: 0,
+                  indent: 0,
+                ),
+                Row(children: <Widget>[
+                  const Icon(FontAwesomeIcons.slack, size: 20),
+                  Text(AppLocalizations.of(context)!.slack,
+                      style: Theme.of(context).textTheme.labelLarge)
+                ]),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Flexible(
+                      child: Text(AppLocalizations.of(context)!.connectSlack,
+                          style: Theme.of(context).textTheme.labelMedium),
+                    ),
+                  ],
+                ),
+                MkButton(
+                  label: AppLocalizations.of(context)!.connect,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
         ),
       );
     }
--- flutter_area/lib/UI/Settings/settings_view.dart
+++ flutter_area/lib/UI/Settings/settings_view.dart
@@ -8,7 +8,6 @@ import '../../Utils/constants.dart';
 import '../ReusableWidgets/mk_background.dart';
 import '../ReusableWidgets/mk_switch.dart';
 
-
 class SettingsView extends StatefulWidget {
   const SettingsView({super.key});
 
@@ -23,69 +22,76 @@ class _SettingsViewState extends State<SettingsView> {
   Widget build(BuildContext context) {
     if (kIsPc) {
       return MkBackground(
-        child: Padding(
-          padding: EdgeInsets.only(
+          child: Padding(
+        padding: EdgeInsets.only(
             top: 45.0.ratioH(),
             left: 137.0.ratioW(),
             right: 137.0.ratioW(),
             bottom: 36.0.ratioH()),
+        child: Column(
+          children: <Widget>[
+            Row(children: <Widget>[
+              Text(AppLocalizations.of(context)!.mySettings,
+                  style: Theme.of(context).textTheme.titleMedium)
+            ]),
+            const Divider(
+              endIndent: 0,
+              indent: 0,
+            ),
+            Row(children: <Widget>[
+              Text(AppLocalizations.of(context)!.appearance,
+                  style: Theme.of(context).textTheme.labelLarge)
+            ]),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(AppLocalizations.of(context)!.changeAppearance,
+                    style: Theme.of(context).textTheme.labelMedium),
+                const SwitchExample(),
+              ],
+            ),
+          ],
+        ),
+      ));
+    } else {
+      return SafeArea(
+        child: MkBackground(
+            child: Padding(
+          padding: EdgeInsets.only(
+              top: 45.0.ratioH(),
+              left: 37.0.ratioW(),
+              right: 37.0.ratioW(),
+              bottom: 36.0.ratioH()),
           child: Column(
             children: <Widget>[
               Row(children: <Widget>[
-                Text(AppLocalizations.of(context)!.mySettings, style: Theme.of(context).textTheme.titleMedium)
-                ]),
+                Text(AppLocalizations.of(context)!.mySettings,
+                    style: Theme.of(context).textTheme.titleMedium)
+              ]),
               const Divider(
-                  endIndent: 0,
-                  indent: 0,
-                ),
+                endIndent: 0,
+                indent: 0,
+              ),
               Row(children: <Widget>[
-                Text(AppLocalizations.of(context)!.appearance, style: Theme.of(context).textTheme.labelLarge)
-                ]),
+                Text(AppLocalizations.of(context)!.appearance,
+                    style: Theme.of(context).textTheme.labelLarge)
+              ]),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Text(AppLocalizations.of(context)!.changeAppearance, style: Theme.of(context).textTheme.labelMedium),
+                  Flexible(
+                    child: Text(
+                      AppLocalizations.of(context)!.changeAppearance,
+                      style: Theme.of(context).textTheme.labelSmall,
+                    ),
+                  ),
+                  SizedBox(width: 10.0.ratioW()),
                   const SwitchExample(),
                 ],
               ),
             ],
           ),
-        )
-      );
-    } else {
-      return MkBackground(
-        child: Padding(
-          padding: EdgeInsets.only(
-            top: 45.0.ratioH(),
-            left: 37.0.ratioW(),
-            right: 37.0.ratioW(),
-            bottom: 36.0.ratioH()),
-          child: Column(
-            children: <Widget>[
-              Row(children: <Widget>[
-                Text(AppLocalizations.of(context)!.mySettings, style: Theme.of(context).textTheme.titleMedium)
-                ]),
-              const Divider(
-                  endIndent: 0,
-                  indent: 0,
-                ),
-              Row(children: <Widget>[
-                Text(AppLocalizations.of(context)!.appearance, style: Theme.of(context).textTheme.labelLarge)
-                ]),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(AppLocalizations.of(context)!.changeAppearance, style: Theme.of(context).textTheme.labelSmall),
-                ],
-              ),
-              const Row(
-                children: <Widget>[
-                  SwitchExample(),
-                ],
-              ),
-            ],
-          ),
-        )
+        )),
       );
     }
   }
