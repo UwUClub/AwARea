6fb63d3dbadffd396c8ee01fe44e52c9a7619760
Valegox <geg.valentin@gmail.com>
2024-01-03 16:38:18 +0100

    Add shared preferences import

--- flutter_area/lib/Core/Manager/user_manager.dart
+++ flutter_area/lib/Core/Manager/user_manager.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../Utils/constants.dart';
 import '../../Utils/mk_print.dart';
