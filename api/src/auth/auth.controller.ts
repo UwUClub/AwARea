9290f6ef7645a7385d55b4deb50fab6c77032602
Quentin Challon <quentin.challon@epitech.eu>
2024-01-14 21:22:32 +0100

    Change something

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,14 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Headers, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './_utils/dto/request/sign-in.dto';
 import { CreateUserDto } from '../users/_utils/dto/request/create-user.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiHeader, ApiTags } from '@nestjs/swagger';
 import { GoogleLoginDto } from './_utils/dto/request/google-login.dto';
+import { UsersService } from '../users/users.service';
+import * as jwt from 'jsonwebtoken';
+import { ConfigService } from '@nestjs/config';
+import { EnvironmentVariables } from '../_utils/config';
+import { JwtStrategy } from './jwt/jwt.startegy';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+    private readonly configService: ConfigService<EnvironmentVariables, true>,
+    private readonly jwtStrategy: JwtStrategy,
+  ) {}
 
   /**
    * Endpoint for user login.
@@ -26,7 +36,15 @@ export class AuthController {
   }
 
   @Post('google-login')
-  googleLogin(@Body() googleLoginDto: GoogleLoginDto) {
+  async googleLogin(@Headers('Authorization') authorizationHeader: string, @Body() googleLoginDto: GoogleLoginDto) {
+    if (authorizationHeader) {
+      const token = authorizationHeader.split(' ')[1];
+      const decodeToken = jwt.verify(token, this.configService.get('JWT_SECRET'));
+      if (!decodeToken) throw new UnauthorizedException('Bad token');
+      const user = await this.jwtStrategy.validate(decodeToken);
+      console.log(user);
+      return this.usersService.updateGoogleToken(user, googleLoginDto.accessToken);
+    }
     return this.authService.connectWithGoogle(googleLoginDto);
   }
 }
